6c77110ae441114d66e3df6cd650e6167fde3f40
Jytesh <44925963+Jytesh@users.noreply.github.com>
2021-02-26 19:25:06 +0530

    Clippy?

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use std::fs;
 
 use threadpool::ThreadPool;
 
@@ -10,13 +10,13 @@ fn main() {
     let streams = TcpListener::bind("localhost:7878").unwrap();
     let pool = ThreadPool::new(4);
     for stream in streams.incoming() {
-        pool.execute(||{
+        pool.execute(|| {
             handle(stream.unwrap());
         })
     }
 }
 
-fn handle (mut stream: TcpStream) {
+fn handle(mut stream: TcpStream) {
     let mut buf = [0; 1024];
     stream.read(&mut buf).unwrap();
     println!("{}", String::from_utf8_lossy(&buf));
@@ -25,8 +25,13 @@ fn handle (mut stream: TcpStream) {
     } else {
         ("404 NOT FOUND", "404.html")
     };
-    let contents = fs::read_to_string("./public/views/".to_owned()+ &file).unwrap();
-    let res = format!("HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",stat, contents.len(), contents);
+    let contents = fs::read_to_string("./public/views/".to_owned() + file).unwrap();
+    let res = format!(
+        "HTTP/1.1 {}\r\nContent-Length: {}\r\n\r\n{}",
+        stat,
+        contents.len(),
+        contents
+    );
     stream.write(res.as_bytes()).unwrap();
-        stream.flush().unwrap()
+    stream.flush().unwrap()
 }
